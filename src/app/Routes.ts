ec07253f01c6b178783b9adfc7dba2f51e51ed92
frank tchatseu <louenkamfrank@gmail.com>
2020-12-12 15:30:47 +0100

    fix access cross origin

--- src/app/Routes.ts
+++ src/app/Routes.ts
@@ -4,4 +4,4 @@ export const SERVER = ROOT + 'api/';
 export const LOGIN = SERVER + 'auth/token';
 export const UPDATE_PASSWORD = SERVER + 'auth/updatepassword';
 
-export const BLOG = SERVER + 'blogs';
+export const BLOG = SERVER + 'blog/article/';
