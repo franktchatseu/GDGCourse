e2afef8bf477de24083acdf6b696cb3893dab4a1
frank tchatseu <louenkamfrank@gmail.com>
2020-12-19 17:31:18 +0100

    i introduced the form (le formulaire) in blogAddComponent

--- src/app/admin/blog/blog-add/blog-add.component.ts
+++ src/app/admin/blog/blog-add/blog-add.component.ts
@@ -1,5 +1,8 @@
+import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { CourseService } from 'src/app/services/course.service';
 
 @Component({
   selector: 'app-blog-add',
@@ -7,11 +10,50 @@ import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
   styleUrls: ['./blog-add.component.scss']
 })
 export class BlogAddComponent implements OnInit {
-
-  constructor() { }
+  public Editor = ClassicEditor ;
+  public blogForm : FormGroup
+  constructor(
+    private formBuilder: FormBuilder,
+    private blogServive : CourseService
+  ) { }
 
   ngOnInit() {
+    this.initForm()
   }
-  public Editor = ClassicEditor ;
 
+  initForm(){
+    this.blogForm = this.formBuilder.group(
+      {
+        'title':['',[Validators.required]],
+        'description':'',
+        'category':'',
+        'body': '',
+        'image':''
+      }
+    )
+  }
+  get form(){
+    return this.blogForm.controls;
+  }
+  onSubmit(){
+    console.log(this.form.body.value)
+    const formData = new FormData()
+    formData.append('title',this.form.title.value)
+    formData.append('description',this.form.description.value)
+    formData.append('category',this.form.category.value)
+    formData.append('body', this.form.body.value)
+    formData.append('banner', this.form.image.value)
+    formData.append('user_id',"1")
+    //creation 
+    this.blogServive.create(formData).then(
+      (data)=>{
+        console.log(data)
+      },
+      (error)=>{
+        console.log(error)
+        console.log("echec ajout de article")
+      }
+    )
+
+  }
 }
--- src/app/admin/blog/blog-view/blog-view.component.ts
+++ src/app/admin/blog/blog-view/blog-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-blog-view',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  addPage(){
+    this.router.navigate(['/admin/blog-add'])
   }
 
 }
