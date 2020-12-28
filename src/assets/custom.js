3424800aaeb17fdd27f61c2f4ddfd5d0708ba9d3
frank tchatseu <louenkamfrank@gmail.com>
2020-12-29 00:01:56 +0100

    delete some image in order to reduce the size of project

--- src/assets/custom.js
+++ src/assets/custom.js
@@ -1,13 +1,41 @@
-var result;
-function saveDocument() {
-    TXTextControl.saveDocument(TXTextControl.StreamType.HTMLFormat,
-        function (e) {
-            console.log(e.data);
-            result = e.data;
-        });
-}
 
-function loadDocument() {
-    TXTextControl.loadDocument(TXTextControl.StreamType.HTMLFormat,
-        btoa('<strong>Test</strong>'));
-}
+(function($) {
+    'use strict';
+
+    // Main Navigation
+    $( '.hamburger-menu' ).on( 'click', function() {
+        $(this).toggleClass('open');
+        $('.site-navigation').toggleClass('show');
+    });
+
+    // Testimonial Slider
+    var swiper = new Swiper('.testimonial-slider', {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        loop: true,
+        effect: 'fade',
+        speed: 800,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        }
+    });
+
+    $('.gallery-wrap').masonry({
+        itemSelector: '.gallery-grid'
+    });
+
+    // Accordion & Toggle
+    $('.accordion-wrap.type-accordion').collapsible({
+        accordion: true,
+        contentOpen: 0,
+        arrowRclass: 'arrow-r',
+        arrowDclass: 'arrow-d'
+    });
+
+    $('.accordion-wrap .entry-title').on('click', function() {
+        $('.accordion-wrap .entry-title').removeClass('active');
+        $(this).addClass('active');
+    });
+
+})(jQuery);
